e46af9154debdc8752e5d14481ef45cc514a894d
Maritza Alas <alas.maritza.j@gmail.com>
2017-10-13 17:27:40 +0000

    user comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,10 @@ class Comment < ApplicationRecord
         'two stars': '2_stars',
         'three stars': '3_stars',
         'four stars': '4_stars',
-        'stars stars': '5_stars',
+        'five stars': '5_stars',
     }
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end
