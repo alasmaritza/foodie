6ea81309080bd8993fb3cfd57011d2247375c29a
Maritza Alas <alas.maritza.j@gmail.com>
2017-10-16 22:59:07 +0000

    image upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
     
     belongs_to :user
     belongs_to :place
+    
+    mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013184401) do
+ActiveRecord::Schema.define(version: 20171016224711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171013184401) do
     t.text     "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "places", force: :cascade do |t|
